e4f0490837afaa51f270b65e0e6065f3a1de6b5c
Paspartout <Paspartout@users.noreply.github.com>
2021-02-07 18:12:02 +0100

    Make ducks drop their feathers and meat, buff saturation

--- common/src/main/java/net/untitledduckmod/registration/ItemSuppliers.java
+++ common/src/main/java/net/untitledduckmod/registration/ItemSuppliers.java
@@ -20,10 +20,10 @@ public class ItemSuppliers {
     public static final Supplier<DuckEggItem> DUCK_EGG = () -> new DuckEggItem(new Item.Settings().maxCount(16).group(ItemGroup.MATERIALS));
 
     public static final Identifier RAW_DUCK_ID = new Identifier(DuckMod.MOD_ID, "raw_duck");
-    public static final Supplier<Item> RAW_DUCK = () -> new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.3F).meat().build()).group(ItemGroup.FOOD));
+    public static final Supplier<Item> RAW_DUCK = () -> new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.4F).meat().build()).group(ItemGroup.FOOD));
 
     public static final Identifier COOKED_DUCK_ID = new Identifier(DuckMod.MOD_ID, "cooked_duck");
-    public static final Supplier<Item> COOKED_DUCK = () -> new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(0.6F).meat().build()).group(ItemGroup.FOOD));
+    public static final Supplier<Item> COOKED_DUCK = () -> new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(0.8F).meat().build()).group(ItemGroup.FOOD));
 
     public static final Identifier DUCK_FEATHER_ID = new Identifier(DuckMod.MOD_ID, "duck_feather");
     public static final Supplier<Item> DUCK_FEATHER = () -> new Item(new Item.Settings().group(ItemGroup.MATERIALS));
