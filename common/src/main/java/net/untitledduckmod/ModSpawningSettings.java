e5b10a8d0058760a9252b464da0f1dc8812a77d6
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-04-28 19:59:44 +0200

    Upgrade geckolib and forge dependencies, increase spawn weights

--- common/src/main/java/net/untitledduckmod/ModSpawningSettings.java
+++ common/src/main/java/net/untitledduckmod/ModSpawningSettings.java
@@ -2,13 +2,13 @@ package net.untitledduckmod;
 
 public class ModSpawningSettings {
     public static class Duck {
-        public static final int WEIGHT = 9;
+        public static final int WEIGHT = 14;
         public static final int MIN_GROUP = 4;
         public static final int MAX_GROUP = 4;
     }
 
     public static class Goose {
-        public static final int WEIGHT = 9;
+        public static final int WEIGHT = 12;
         public static final int MIN_GROUP = 4;
         public static final int MAX_GROUP = 4;
     }
