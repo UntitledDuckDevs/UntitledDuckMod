c0cee0ba6cc03c5a9d64f3f0de21166be4e4895b
Frank Bauer <developer@ambertation.de>
2021-06-30 20:05:47 +0200

    Removed unused import

--- fabric/src/main/java/net/untitledduckmod/fabric/DuckModFabricClientSetup.java
+++ fabric/src/main/java/net/untitledduckmod/fabric/DuckModFabricClientSetup.java
@@ -2,7 +2,6 @@ package net.untitledduckmod.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import net.untitledduckmod.ModEntityTypes;
 import net.untitledduckmod.fabric.duck.DuckRenderer;
