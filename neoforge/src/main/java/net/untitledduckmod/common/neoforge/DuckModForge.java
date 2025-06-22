1846207559ce3362d0b60ce3887cb875099a6f01
Xebeth <github@north-edge.com>
2025-06-22 21:22:01 +0100

    [1.21.6] [Neoforge] Dirty fix so it compiles (untested)

--- neoforge/src/main/java/net/untitledduckmod/common/neoforge/DuckModForge.java
+++ neoforge/src/main/java/net/untitledduckmod/common/neoforge/DuckModForge.java
@@ -30,7 +30,6 @@ public class DuckModForge {
         RegistryHelperImpl.POTIONS.register(bus);
     }
 
-    @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
     public static class ModSetup {
         @SubscribeEvent
         public static void commonSetup(FMLCommonSetupEvent event) {
