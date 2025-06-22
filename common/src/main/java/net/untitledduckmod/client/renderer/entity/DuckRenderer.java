ebf9ebcd5d394eb54f3f1e18bd51828d68135ea1
Xebeth <github@north-edge.com>
2025-06-22 21:33:12 +0100

    [1.21.6] [DuckMod] Update the renderers

--- common/src/main/java/net/untitledduckmod/client/renderer/entity/DuckRenderer.java
+++ common/src/main/java/net/untitledduckmod/client/renderer/entity/DuckRenderer.java
@@ -1,53 +1,11 @@
 package net.untitledduckmod.client.renderer.entity;
 
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.item.HeldItemRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ModelTransformationMode;
-import net.minecraft.util.math.RotationAxis;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.untitledduckmod.client.model.DuckModel;
 import net.untitledduckmod.common.entity.DuckEntity;
-import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib.cache.object.BakedGeoModel;
-import software.bernie.geckolib.cache.object.GeoBone;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.base.GeoRenderState;
 
-public class DuckRenderer extends GeoEntityRenderer<DuckEntity> {
-    private final HeldItemRenderer heldItemRenderer;
-    public DuckRenderer(EntityRendererFactory.Context context) {
-        super(context, new DuckModel());
-        this.shadowRadius = 0.3f;
-        this.heldItemRenderer = context.getRenderDispatcher().getHeldItemRenderer();
-    }
-
-    @Override
-    public void preRender(MatrixStack poseStack, DuckEntity animatable, BakedGeoModel model, @Nullable VertexConsumerProvider bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
-        if (animatable.isBaby()) {
-            this.withScale(0.7f);
-        } else {
-            this.withScale(1.0f);
-        }
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
-    }
-
-    @Override
-    public void renderRecursively(MatrixStack poseStack, DuckEntity animatable, GeoBone bone, RenderLayer renderType, VertexConsumerProvider bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
-        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
-        ItemStack mainHand = animatable.getMainHandStack();
-        if (bone.getName().equals("beak") && !mainHand.isEmpty()) {
-            poseStack.push();
-            poseStack.translate(0.0, 0.50, -0.40);
-
-            poseStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90f));
-            poseStack.scale(0.7f, 0.7f, 0.7f);
-
-            heldItemRenderer.renderItem(animatable, mainHand, ModelTransformationMode.GROUND, false, poseStack, bufferSource, packedLight);
-            poseStack.pop();
-
-        }
-    }
+public class DuckRenderer<R extends LivingEntityRenderState & GeoRenderState> extends WaterfowlRenderer<DuckEntity, R> {
+    public DuckRenderer(EntityRendererFactory.Context context) { super(new DuckModel(), context); }
 }
