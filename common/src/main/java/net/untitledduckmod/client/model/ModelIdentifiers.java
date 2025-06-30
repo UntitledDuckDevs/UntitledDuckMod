1b02a018736fa1aefcf1ba3a9c1a1b1ba93e830a
Suel_ki <1829446054@qq.com>
2025-06-30 15:49:57 +0800

    feat(models): add a base class for models and use DefaultedEntityGeoModel to implement the head turning animations

--- common/src/main/java/net/untitledduckmod/client/model/ModelIdentifiers.java
+++ common/src/main/java/net/untitledduckmod/client/model/ModelIdentifiers.java
@@ -4,14 +4,10 @@ import net.minecraft.util.Identifier;
 import net.untitledduckmod.DuckMod;
 
 public class ModelIdentifiers {
-    public static final Identifier DUCK_MODEL_LOCATION = DuckMod.id("geo/duck.geo.json");
-    public static final Identifier DUCK_ANIMATION_FILE_LOCATION = DuckMod.id("animations/duck.animation.json");
     public static final Identifier NORMAL_TEXTURE = DuckMod.id("textures/entity/duck.png");
     public static final Identifier FEMALE_TEXTURE = DuckMod.id("textures/entity/duck_female.png");
     public static final Identifier DUCKLING_TEXTURE = DuckMod.id("textures/entity/duckling.png");
 
-    public static final Identifier GOOSE_MODEL_LOCATION = DuckMod.id("geo/goose.geo.json");
-    public static final Identifier GOOSE_ANIMATION_FILE_LOCATION = DuckMod.id("animations/goose.animation.json");
     public static final Identifier GOOSE_TEXTURE = DuckMod.id("textures/entity/goose.png");
     public static final Identifier CANADIAN_GOOSE_TEXTURE = DuckMod.id("textures/entity/canadian_goose.png");
     public static final Identifier GOSLING_TEXTURE = DuckMod.id("textures/entity/gosling.png");
