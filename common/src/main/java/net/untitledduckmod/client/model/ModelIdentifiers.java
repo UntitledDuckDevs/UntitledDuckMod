e75cbd62626d78850f1d8d02847f99ed3e459cd7
Suel_ki <1829446054@qq.com>
2025-07-09 21:31:19 +0800

    feat(entity):Add 2 duck variants (Pekin & Kakhi Campbell)

--- common/src/main/java/net/untitledduckmod/client/model/ModelIdentifiers.java
+++ common/src/main/java/net/untitledduckmod/client/model/ModelIdentifiers.java
@@ -6,6 +6,9 @@ import net.untitledduckmod.DuckMod;
 public class ModelIdentifiers {
     public static final Identifier NORMAL_TEXTURE = DuckMod.id("textures/entity/duck.png");
     public static final Identifier FEMALE_TEXTURE = DuckMod.id("textures/entity/duck_female.png");
+    public static final Identifier PEKIN_TEXTURE = DuckMod.id("textures/entity/duck_pekin.png");
+    public static final Identifier CAMPBELL_TEXTURE = DuckMod.id("textures/entity/duck_campbell.png");
+
     public static final Identifier DUCKLING_TEXTURE = DuckMod.id("textures/entity/duckling.png");
 
     public static final Identifier GOOSE_TEXTURE = DuckMod.id("textures/entity/goose.png");
