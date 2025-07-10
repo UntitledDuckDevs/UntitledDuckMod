42c5c542899616b468096e29e92ca30639cc6fe9
Suel_ki <1829446054@qq.com>
2025-07-11 00:18:31 +0800

    refactor: separate CustomSpawnGroup implementations for Forge and Fabric

--- forge/src/main/java/net/untitledduckmod/mixin/forge/SpawnGroupMixin.java
+++ forge/src/main/java/net/untitledduckmod/mixin/forge/SpawnGroupMixin.java
@@ -1,4 +1,4 @@
-package net.untitledduckmod.mixin;
+package net.untitledduckmod.mixin.forge;
 
 import net.minecraft.entity.SpawnGroup;
 import net.untitledduckmod.common.entity.CustomSpawnGroup;
@@ -6,8 +6,6 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Arrays;
 
 // credit to hybrid aquatic for the code
 @SuppressWarnings("unused")
@@ -18,27 +16,25 @@ public class SpawnGroupMixin {
         throw new AssertionError();
     }
 
-    @Shadow
-    @Mutable
-    @Final
-    private static SpawnGroup[] field_6301;
-
     @Unique
     private static SpawnGroup untitledDuck$createSpawnGroup(String enumname, int ordinal, CustomSpawnGroup spawnGroup) {
         return ((SpawnGroup)(Object) new SpawnGroupMixin(enumname, ordinal, spawnGroup.name, spawnGroup.spawnCap, spawnGroup.peaceful, spawnGroup.rare, spawnGroup.immediateDespawnRange));
     }
 
-    @Inject(method = "<clinit>", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/SpawnGroup;field_6301:[Lnet/minecraft/entity/SpawnGroup;", shift = At.Shift.AFTER))
+    @Inject(method = "<clinit>", at = @At(value = "TAIL"))
     private static void addGroups(CallbackInfo ci) {
-        int vanillaSpawnGroupsLength = field_6301.length;
-        CustomSpawnGroup[] groups = CustomSpawnGroup.values();
-        field_6301 = Arrays.copyOf(field_6301, vanillaSpawnGroupsLength + groups.length);
-
-        for(int i = 0; i < groups.length; i++) {
-            int pos = vanillaSpawnGroupsLength + i;
-            CustomSpawnGroup spawnGroup = groups[i];
-            spawnGroup.spawnGroup = field_6301[pos] = untitledDuck$createSpawnGroup(spawnGroup.name(), pos, spawnGroup);
+        var spawnGroup = SpawnGroup.byName(CustomSpawnGroup.WATERFOWL.name);
+        if (spawnGroup == null) {
+            spawnGroup = SpawnGroup.create(
+                    CustomSpawnGroup.WATERFOWL_NAME,
+                    CustomSpawnGroup.WATERFOWL_NAME,
+                    CustomSpawnGroup.WATERFOWL_SPAWN_CAP,
+                    CustomSpawnGroup.WATERFOWL_PEACEFUL,
+                    CustomSpawnGroup.WATERFOWL_RARE,
+                    CustomSpawnGroup.WATERFOWL_IMMEDIATE_DESPAWN_RANGE
+            );
         }
+        CustomSpawnGroup.WATERFOWL.spawnGroup = spawnGroup;
     }
 
 }
