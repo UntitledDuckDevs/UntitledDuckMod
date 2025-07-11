6db8126e2434b75b4bdb90fdd5bbb87074273b64
Suel_ki <1829446054@qq.com>
2025-07-11 22:20:11 +0800

    feat(spawn):add CustomSpawnGroup

--- neoforge/src/main/java/net/untitledduckmod/common/platform/neoforge/RegistryHelperImpl.java
+++ neoforge/src/main/java/net/untitledduckmod/common/platform/neoforge/RegistryHelperImpl.java
@@ -1,9 +1,7 @@
 package net.untitledduckmod.common.platform.neoforge;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.component.ComponentType;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.Item;
@@ -16,6 +14,7 @@ import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.untitledduckmod.DuckMod;
+import net.untitledduckmod.common.entity.CustomSpawnGroup;
 import net.untitledduckmod.common.entity.neoforge.DuckEntityForge;
 import net.untitledduckmod.common.entity.neoforge.GooseEntityForge;
 
@@ -27,13 +26,13 @@ public class RegistryHelperImpl {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(Registries.SOUND_EVENT, DuckMod.MOD_ID);
     public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(Registries.POTION, DuckMod.MOD_ID);
     public static final DeferredRegister<StatusEffect> STATUS_EFFECTS = DeferredRegister.create(Registries.STATUS_EFFECT, DuckMod.MOD_ID);
-    public static final DeferredRegister<ComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.create(Registries.DATA_COMPONENT_TYPE, DuckMod.MOD_ID);
     public static final DeferredRegister<MapCodec<? extends BiomeModifier>> BIOME_MODIFIERS = DeferredRegister.create(NeoForgeRegistries.Keys.BIOME_MODIFIER_SERIALIZERS, DuckMod.MOD_ID);
 
     public static <T extends Item> Supplier<T> registerItem(String name, Supplier<T> item) {
         return ITEMS.register(name, item);
     }
 
+    @SuppressWarnings("unchecked")
     public static <T extends Item> Supplier<T> registerSpawnEggItem(
             String name, Supplier<? extends EntityType<? extends MobEntity>> type,
             int primaryColor, int secondaryColor, Item.Settings settings
@@ -41,11 +40,12 @@ public class RegistryHelperImpl {
         return (Supplier<T>) registerItem(name, () -> new DeferredSpawnEggItem(type, primaryColor, secondaryColor, settings));
     }
 
+    @SuppressWarnings("unchecked")
     public static  <T extends EntityType<?>> Supplier<T> registerEntity(String name, Supplier<T> entityType) {
         if (name.equals("duck")) {
-            entityType = () -> (T) EntityType.Builder.create(DuckEntityForge::new, SpawnGroup.CREATURE).dimensions(0.6f, 0.6f).maxTrackingRange(10).build(DuckMod.stringID("duck"));
+            entityType = () -> (T) EntityType.Builder.create(DuckEntityForge::new, CustomSpawnGroup.WATERFOWL.spawnGroup).dimensions(0.6f, 0.6f).maxTrackingRange(10).build(DuckMod.stringID("duck"));
         } else if (name.equals("goose")) {
-            entityType = () -> (T) EntityType.Builder.create(GooseEntityForge::new, SpawnGroup.CREATURE).dimensions(0.7f, 1.2f).maxTrackingRange(10).build(DuckMod.stringID("goose"));
+            entityType = () -> (T) EntityType.Builder.create(GooseEntityForge::new, CustomSpawnGroup.WATERFOWL.spawnGroup).dimensions(0.7f, 1.2f).maxTrackingRange(10).build(DuckMod.stringID("goose"));
         }
         return ENTITY_TYPES.register(name, entityType);
     }
